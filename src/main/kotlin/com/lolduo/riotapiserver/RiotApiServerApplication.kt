e1c2a633bca428f07f0ec8462e71a2d97836d112
남윤건 <nam-yungeon@nam-yungeon-ui-MacBookPro.local>
2023-02-09 23:22:42 +0900

    [FEAT] 기본 spring boot 2.9.7 & mongoDB reactive

---
+++ src/main/kotlin/com/lolduo/riotapiserver/RiotApiServerApplication.kt
@@ -0,0 +1,13 @@
+package com.lolduo.riotapiserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RiotApiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<RiotApiServerApplication>(*args)
+
+
+}
