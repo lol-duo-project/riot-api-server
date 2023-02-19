67b65a4993e0e2aa0ab7b8fc60b6c0e62a21257a
imygnam <imygnam@gmail.com>
2023-02-19 12:45:50 +0900

    [FEAT] champion info data class 생성

---
+++ src/main/kotlin/com/lolduo/riotapiserver/service/riotApi/response/champion/Image.kt
@@ -0,0 +1,11 @@
+package com.lolduo.riotapiserver.service.riotApi.response.champion
+
+data class Image(
+    val full: String,
+    val sprite: String,
+    val group: String,
+    val x: Int,
+    val y: Int,
+    val w: Int,
+    val h: Int
+)
